dfbc383a92e6f22ecbc615265b6f572e956fdd37
Jonathan Killen <Productionmax@hotmail.com>
2022-08-02 10:56:52 +0800

    Added a slider in HTML and CSS

--- logic.js
+++ logic.js
@@ -1,8 +1,13 @@
 const container = document.querySelector('.container');
-const square = document.createElement('div');
+const value = document.querySelector('#rangeValue');
+const slider = document.querySelector('.rangeSlider');
 let cell;
 let target;
+slider.oninput = function (){
+    value.innerHTML = this.value;
+}
 
+// The number of cells being made
 function makeCell(numCell){
     for (let i=0;i < numCell;i++){
         cell = document.createElement('div');
@@ -10,3 +15,24 @@ function makeCell(numCell){
         container.appendChild(cell);
     }
 }
+//Set property changes this (variable, new value)
+//Structures the grid 
+function makeGrid(numRow,numCol){
+    container.style.setProperty('--numRow',numRow);
+    container.style.setProperty('--numCol',numCol);
+    for (let i=0;i<numCol;i++){
+        makeCell(numRow);
+    }
+}
+
+function chooseSize(numGrid){
+    slider.oninput = function (){
+        value.innerHTML = this.value;
+        numGrid = value.innerHTML;
+        makeGrid(numGrid,numGrid);
+    }
+    
+    
+}
+
+chooseSize();
